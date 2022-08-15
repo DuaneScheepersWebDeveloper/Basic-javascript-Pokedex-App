05e00f182d9d2a6aefc64eeef76da6b80eae9397
duaneo2011 <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2022-08-15 13:27:26 +0200

    Able to display All the Kanto Pokemon on screen
    Able to display All the Kanto Pokemon on screen

--- Static/javascript/pokedex.js
+++ Static/javascript/pokedex.js
@@ -1,5 +1,6 @@
 document.querySelector('#search').addEventListener('click', getPokemon);
 
+//---------------------------------------------------------------------
 const capitalizeFirstLetter = (string) => {
 	return string.charAt(0).toUpperCase() + string.slice(1);
 };
@@ -7,7 +8,8 @@ const capitalizeFirstLetter = (string) => {
 const lowerCaseName = (string) => {
 	return string.toLowerCase();
 };
-
+//---------------------------------------------------------------------
+//Get a single Pokemon
 function getPokemon(e) {
 	const name = document.querySelector('#pokemonName').value;
 	const pokemonName = lowerCaseName(name);
@@ -27,7 +29,7 @@ function getPokemon(e) {
 	  <p class="idClass">Pokedex Id: ${data.id}</p>
         <h1>${capitalizeFirstLetter(data.name)}</h3>
 		
-        <p class="types">Type: ${data.types
+        <p id="types"class="types">Type: ${data.types
 					.map((type) => type.type.name)
 					.join(' / ')}</p>
 		<p>Weight: ${data.weight}</p>
@@ -54,12 +56,56 @@ function getPokemon(e) {
 
 	e.preventDefault();
 }
+//---------------------------------------------------------------------
+const pokedex = document.getElementById('pokedex');
 
-const fetchKantoPokemon = () => {
-	fetch('https://pokeapi.co/api/v2/pokemon/151/')
-		.then((response) => response.json())
-		.then((allpokemon) => console.log(allpokemon));
+const fetchPokemonKanto = () => {
+	const promises = [];
+	for (let i = 1; i <= 150; i++) {
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+		promises.push(fetch(url).then((res) => res.json()));
+	}
+	Promise.all(promises).then((results) => {
+		const pokemon = results.map((result) => ({
+			name: result.name,
+			image: result.sprites.other['official-artwork'].front_default,
+			type: result.types.map((type) => type.type.name).join(' / '),
+			id: result.id,
+		}));
+		displayPokemon(pokemon);
+	});
 };
+
+const displayPokemon = (pokemon) => {
+	console.log(pokemon);
+	const pokemonHTMLString = pokemon
+		.map(
+			(pokeman) => `
+       <div class="pokemonCard">
+	   <div class="pokemonCardImage">
+            <img class="card-image" src="${pokeman.image}"/>
+			</div>
+			<div class="pokemonInfos">
+			<p>Id:${pokeman.id}</p>
+            <h2 class="card-title">${capitalizeFirstLetter(pokeman.name)}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+			</div>
+      </div>
+    `
+		)
+		.join('');
+	pokeContainer.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemonKanto();
+
+//---------------------------------------------------------------------
+// const fetchKantoPokemon =()=> {
+// 	fetch('https://pokeapi.co/api/v2/pokemon/151/')
+// 		.then((response) => response.json())
+// 		.then((allpokemon) => console.log(allpokemon));
+// }
+//---------------------------------------------------------------------
 
 //905 pokemon
 //Generation I 1-151
@@ -70,11 +116,5 @@ const fetchKantoPokemon = () => {
 //Generation VI 650-721
 //Generation VII 722-809
 //Generation VIII 810-905
-//bulbasaur poison grass
-//mew Normal
-
-// const fetchKantoPokemon = () => {
-// 	fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-// 		.then((response) => response.json())
-// 		.then((allpokemon) => console.log(allpokemon));
-// };
+//Bulbasaur poison grass
+//Mew Normal
