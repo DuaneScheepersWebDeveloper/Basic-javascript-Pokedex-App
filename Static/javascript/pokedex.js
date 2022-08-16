16aead5f61133ff3ef1b63cd7f50a7a65c22cdbb
duaneo2011 <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2022-08-16 10:17:03 +0200

    Region functionality added
    Region functionality added

--- Static/javascript/pokedex.js
+++ Static/javascript/pokedex.js
@@ -1,5 +1,7 @@
 document.querySelector('#search').addEventListener('click', getPokemon);
-
+document.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('region').addEventListener('input', handleSelect);
+});
 //---------------------------------------------------------------------
 const capitalizeFirstLetter = (string) => {
 	return string.charAt(0).toUpperCase() + string.slice(1);
@@ -8,7 +10,7 @@ const capitalizeFirstLetter = (string) => {
 const lowerCaseName = (string) => {
 	return string.toLowerCase();
 };
-//---------------------------------------------------------------------
+
 //Get a single Pokemon
 function getPokemon(e) {
 	const name = document.querySelector('#pokemonName').value;
@@ -57,23 +59,144 @@ function getPokemon(e) {
 	e.preventDefault();
 }
 //---------------------------------------------------------------------
-
+//Displaying multiple Pokemon
+//Kanto Gen 1
 const fetchPokemonKanto = () => {
 	const promises = [];
 	for (let i = 1; i <= 150; i++) {
 		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
 		promises.push(fetch(url).then((res) => res.json()));
 	}
-	Promise.all(promises).then((results) => {
-		const pokemon = results.map((result) => ({
-			name: result.name,
-			image: result.sprites.other['official-artwork'].front_default,
-			type: result.types.map((type) => type.type.name).join(' / '),
-			id: result.id,
+	Promise.all(promises).then((data) => {
+		const pokemon = data.map((data) => ({
+			name: data.name,
+			image: data.sprites.other['official-artwork'].front_default,
+			type: data.types.map((type) => type.type.name).join(' / '),
+			id: data.id,
 		}));
 		displayPokemon(pokemon);
 	});
 };
+//---------------------------------------------------------------------
+const fetchPokemonJohto = () => {
+	const promises = [];
+	for (let i = 152; i <= 251; i++) {
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+		promises.push(fetch(url).then((res) => res.json()));
+	}
+	Promise.all(promises).then((data) => {
+		const pokemon = data.map((data) => ({
+			name: data.name,
+			image: data.sprites.other['official-artwork'].front_default,
+			type: data.types.map((type) => type.type.name).join(' / '),
+			id: data.id,
+		}));
+		displayPokemon(pokemon);
+	});
+};
+//---------------------------------------------------------------------
+const fetchPokemonHoenn = () => {
+	const promises = [];
+	for (let i = 252; i <= 386; i++) {
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+		promises.push(fetch(url).then((res) => res.json()));
+	}
+	Promise.all(promises).then((data) => {
+		const pokemon = data.map((data) => ({
+			name: data.name,
+			image: data.sprites.other['official-artwork'].front_default,
+			type: data.types.map((type) => type.type.name).join(' / '),
+			id: data.id,
+		}));
+		displayPokemon(pokemon);
+	});
+};
+//---------------------------------------------------------------------
+const fetchPokemonSinnoh = () => {
+	const promises = [];
+	for (let i = 387; i <= 649; i++) {
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+		promises.push(fetch(url).then((res) => res.json()));
+	}
+	Promise.all(promises).then((data) => {
+		const pokemon = data.map((data) => ({
+			name: data.name,
+			image: data.sprites.other['official-artwork'].front_default,
+			type: data.types.map((type) => type.type.name).join(' / '),
+			id: data.id,
+		}));
+		displayPokemon(pokemon);
+	});
+};
+//---------------------------------------------------------------------
+const fetchPokemonUnova = () => {
+	const promises = [];
+	for (let i = 494; i <= 649; i++) {
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+		promises.push(fetch(url).then((res) => res.json()));
+	}
+	Promise.all(promises).then((data) => {
+		const pokemon = data.map((data) => ({
+			name: data.name,
+			image: data.sprites.other['official-artwork'].front_default,
+			type: data.types.map((type) => type.type.name).join(' / '),
+			id: data.id,
+		}));
+		displayPokemon(pokemon);
+	});
+};
+//---------------------------------------------------------------------
+const fetchPokemonKalos = () => {
+	const promises = [];
+	for (let i = 650; i <= 721; i++) {
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+		promises.push(fetch(url).then((res) => res.json()));
+	}
+	Promise.all(promises).then((data) => {
+		const pokemon = data.map((data) => ({
+			name: data.name,
+			image: data.sprites.other['official-artwork'].front_default,
+			type: data.types.map((type) => type.type.name).join(' / '),
+			id: data.id,
+		}));
+		displayPokemon(pokemon);
+	});
+};
+//---------------------------------------------------------------------
+const fetchPokemonAlola = () => {
+	const promises = [];
+	for (let i = 722; i <= 809; i++) {
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+		promises.push(fetch(url).then((res) => res.json()));
+	}
+	Promise.all(promises).then((data) => {
+		const pokemon = data.map((data) => ({
+			name: data.name,
+			image: data.sprites.other['official-artwork'].front_default,
+			type: data.types.map((type) => type.type.name).join(' / '),
+			id: data.id,
+		}));
+		displayPokemon(pokemon);
+	});
+};
+//---------------------------------------------------------------------
+const fetchPokemonGalar = () => {
+	const promises = [];
+	for (let i = 810; i <= 905; i++) {
+		const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+		promises.push(fetch(url).then((res) => res.json()));
+	}
+	Promise.all(promises).then((data) => {
+		const pokemon = data.map((data) => ({
+			name: data.name,
+			image: data.sprites.other['official-artwork'].front_default,
+			type: data.types.map((type) => type.type.name).join(' / '),
+			id: data.id,
+		}));
+		displayPokemon(pokemon);
+	});
+};
+//---------------------------------------------------------------------
 
 const displayPokemon = (pokemon) => {
 	console.log(pokemon);
@@ -96,24 +219,56 @@ const displayPokemon = (pokemon) => {
 	pokeContainer.innerHTML = pokemonHTMLString;
 };
 
-fetchPokemonKanto();
+function handleSelect(ev) {
+	let select = ev.target;
+	switch (select.value) {
+		case 'Kanto':
+			fetchPokemonKanto();
+			break;
+		case 'Johto':
+			fetchPokemonJohto();
+			break;
+		case 'Hoenn':
+			fetchPokemonHoenn();
+			break;
+		case 'Sinnoh':
+			fetchPokemonSinnoh();
+			break;
+		case 'Unova':
+			fetchPokemonUnova();
+			break;
+		case 'Kalos':
+			fetchPokemonKalos();
+			break;
+		case 'Alola':
+			fetchPokemonAlola();
+			break;
+		case 'Galar':
+			fetchPokemonGalar();
+			break;
+		default:
+			fetchPokemonKanto();
+	}
+}
 
 //---------------------------------------------------------------------
-// const fetchKantoPokemon =()=> {
-// 	fetch('https://pokeapi.co/api/v2/pokemon/151/')
-// 		.then((response) => response.json())
-// 		.then((allpokemon) => console.log(allpokemon));
-// }
+const fetchMew = () => {
+	fetch('https://pokeapi.co/api/v2/pokemon/151/')
+		.then((response) => response.json())
+		.then((allpokemon) => console.log(allpokemon));
+};
 //---------------------------------------------------------------------
 
 //905 pokemon
-//Generation I 1-151
-//Generation II 152-251
-//Generation III 252-386
-//Generation IV 387-493
-//Generation V 494-649
-//Generation VI 650-721
-//Generation VII 722-809
-//Generation VIII 810-905
+//Generation I 1-151 Kanto
+//Generation II 152-251 Johto
+//Generation III 252-386 Hoenn
+//Generation IV 387-493 Sinnoh
+//Generation V 494-649 Unova
+//Generation VI 650-721 Kalos
+//Generation VII 722-809 Alola
+//Generation VIII 810-905 Galar
 //Bulbasaur poison grass
 //Mew Normal
+
+//---------------------------------------------------------------------
